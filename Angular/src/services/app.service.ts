e51076473690684b8866e27cc79f8570fa6e7b84
Drew Miller <drewmiller@Icarus-OSX.local>
2021-03-17 18:00:08 -0700

    enabled security for applications in release configuration

--- Angular/src/services/app.service.ts
+++ Angular/src/services/app.service.ts
@@ -9,7 +9,7 @@ import { Greeting } from 'src/models/greeting';
 export class AppService {
   private serviceEndpoint = environment.serviceEndpoint;
   private functionEndpoint = environment.functionEndpoint;
-
+  private functionCode = 'MwNtob2zm8tBaV2T6R8c/h8ma/UUpN62BlPxamqk94mtEK0a5paSSg==';
   private functionOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'text/plain; charset=utf-8' }),
     responseType: 'text' as 'json'
@@ -28,11 +28,14 @@ export class AppService {
   }
 
   public GetFunction(query: string = 'Azure'): Observable<any> {
-    const url = this.functionEndpoint + `/api/DemoTrigger?name=${this.SanitizeQuery(query)}`;
+    const url = this.functionEndpoint + `/api/DemoTrigger/?code=${this.SanitizeQuery(this.functionCode)}&name=${this.SanitizeQuery(query)}`;
     return this.httpClient.get<any>(url, this.functionOptions);
   }
 
   private SanitizeQuery(x: string) {
-    return x.trim().split(' ').join('%20');
+    return x.trim()
+      .replace(' ', '%20')
+      .replace('/', '%2F')
+      .replace('=', '%3D');
   }
 }
