79bd2bbf49514a860376000994321aaa68b9b8f3
Drew Miller <d.m.lespaul@gmail.com>
2021-03-18 18:13:33 -0700

    updated some code to try to run cosmos on program main

--- Data/Models/demo-model.cs
+++ Data/Models/demo-model.cs
@@ -2,7 +2,7 @@ using Newtonsoft.Json;
 
 namespace Data.Models
 {
-    public class Model
+    public class DemoModel
     {
         public string Id { get; set; }
         public string Value { get; set; }
--- Data/Program.cs
+++ Data/Program.cs
@@ -7,20 +7,20 @@ using Data.Models;
 
 namespace Data
 {
-    // The Azure Cosmos DB endpoint for running this sample.
-    private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"];
-
-    // The primary key for the Azure Cosmos account.
-    private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
-
     class Program
     {
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"];
+
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
+
         public static async Task Main(string[] args)
         {
             // await Demo.Run();
-            var dbClient = new DbClient(EndpointUri, PrimaryKey, 'CosmosDemoDb');
-            var container = dbClient.CreateContainer();
-            Itemresponse<string> response = await container.CreateItemAsync<Model>('Hello World!');
+            var dbClient = new DbClient(EndpointUri, PrimaryKey, "CosmosDemoDb");
+            var container = await dbClient.CreateContainer();
+            Itemresponse<DemoModel> response = await container.CreateItemAsync<DemoModel>("Hello World!");
         }
     }
 }
